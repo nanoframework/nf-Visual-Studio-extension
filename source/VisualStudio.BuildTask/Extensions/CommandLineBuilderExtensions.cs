1877473fbacb88c732851269a7e2e3d3ca69cd57
José Simões <josesimoes@users.noreply.github.com>
2017-04-26 14:14:14 +0100

    Add VS2015 solution for MSBuild tasks (#13)
    - had to implement these in VS2015 solution to make it easier to build the Nuget package (in the future, all this should be moved to VS 2017)
    - update Nuget package to include the supporting MSBuild tasks, project targets and suport tools
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/VisualStudio.BuildTask/Extensions/CommandLineBuilderExtensions.cs
@@ -0,0 +1,73 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace nanoFramework.Tools.VisualStudio
+{
+    public static class CommandLineBuilderExtensions
+    {
+        public static void AppendSwitchIfTrue(this CommandLineBuilder commandLinedBuilder, string switchValue, bool value)
+        {
+            if(value)
+            {
+                commandLinedBuilder.AppendSwitch("-minimize");
+            }
+        }
+
+        public static void AppendSwitchAndFiles(this CommandLineBuilder commandLinedBuilder, string switchName, ITaskItem[] fileList)
+        {
+            // paranoid check if property has any data 
+            if (fileList?.Length > 0)
+            {
+                commandLinedBuilder.AppendSwitch(switchName);
+
+                foreach(ITaskItem file in fileList)
+                {
+                    commandLinedBuilder.AppendFileNameIfNotNull(file);
+                };
+            }
+        }
+
+        public static void AppendSwitchForEachFile(this CommandLineBuilder commandLinedBuilder, string switchName, ITaskItem[] fileList)
+        {
+            if (fileList?.Length > 0)
+            {
+
+                foreach (ITaskItem file in fileList)
+                {
+                    commandLinedBuilder.AppendSwitch(switchName);
+                    commandLinedBuilder.AppendFileNameIfNotNull(file.ItemSpec);
+                };
+            }
+        }
+
+        public static void AppendSwitchForFile(this CommandLineBuilder commandLinedBuilder, string switchName, string fileName)
+        {
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                commandLinedBuilder.AppendSwitch(switchName);
+                commandLinedBuilder.AppendFileNameIfNotNull(fileName);
+            }
+        }
+
+        //public static void AppendSwitchToFileAndExtraSwitches(this CommandLineBuilder commandLinedBuilder, string switchValue, string file, )
+        //{
+        //    // paranoid check if property has any data 
+        //    if (fileList?[0]?.MetadataCount > 0)
+        //    {
+        //        commandLinedBuilder.AppendSwitch(switchValue);
+
+        //        fileList.Select(stringFile => {
+
+        //            commandLinedBuilder.AppendFileNameIfNotNull(stringFile);
+
+        //            return new object();
+        //        }).ToList();
+        //    }
+        //}
+    }
+}
