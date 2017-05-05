44ab6067a6fac4a1e1801c4ff4db5a14e3587641
José Simões <josesimoes@users.noreply.github.com>
2017-05-05 17:02:55 +0100

    Rework VS extension solution (#51)
    - change Nuproj to use targets
    - add all required projects to a single solution to allow local build
    - adjust build sequence to acomplish a sucesfull local build
    - rework individual projects to allow sequenced build on AppVeyor
    - update CorLib Nuget package in templates
    - bumped VS extension version to 0.1.14
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/VisualStudio.Extension/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Extension/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.10.0")]
+[assembly: AssemblyFileVersion("0.1.14.0")]
