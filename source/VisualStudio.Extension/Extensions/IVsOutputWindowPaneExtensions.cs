c992fc6f84f5bf2cbcd02f9cad323d971c683321
José Simões <jose.simoes@eclo.solutions>
2017-06-13 14:12:40 +0100

    Improve string manipulation
    - add resource file for strings
    - add extension to write string to output pane adding a new line at the end
    - update code to use this extension
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/VisualStudio.Extension/Extensions/IVsOutputWindowPaneExtensions.cs
@@ -0,0 +1,23 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+
+namespace nanoFramework.Tools.VisualStudio.Extension
+{
+    public static class IVsOutputWindowPaneExtensions
+    {
+        /// <summary>
+        /// Writes text to the output window pane followed by a line terminator
+        /// </summary>
+        /// <param name="pane"></param>
+        /// <param name="pszOutputString">Text to be appended to the output window pane.</param>
+        public static void OutputStringAsLine(this IVsOutputWindowPane pane, string pszOutputString)
+        {
+            pane.OutputString(pane + Environment.NewLine);
+        }
+    }
+}
