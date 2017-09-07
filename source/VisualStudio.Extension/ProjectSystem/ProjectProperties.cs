edcd90741eef5a82e97ab8e1675195ff4e17617c
José Simões <jose.simoes@eclo.solutions>
2017-09-07 09:49:43 +0100

    Revert Nuget packages
    - updating to VSProjectSystem 15.3 caused the Deploy option to vanish. The cause for this is an aparent regression on their code. Issue reported here https://github.com/Microsoft/VSProjectSystem/issues/247
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/VisualStudio.Extension/ProjectSystem/ProjectProperties.cs
+++ source/VisualStudio.Extension/ProjectSystem/ProjectProperties.cs
@@ -14,6 +14,39 @@ namespace nanoFramework.Tools.VisualStudio.Extension
     [ExcludeFromCodeCoverage]
     internal partial class ProjectProperties : StronglyTypedPropertyAccess
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectProperties"/> class.
+        /// </summary>
+        [ImportingConstructor]
+        public ProjectProperties(ConfiguredProject configuredProject)
+            : base(configuredProject)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectProperties"/> class.
+        /// </summary>
+        public ProjectProperties(ConfiguredProject configuredProject, string file, string itemType, string itemName)
+            : base(configuredProject, file, itemType, itemName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectProperties"/> class.
+        /// </summary>
+        public ProjectProperties(ConfiguredProject configuredProject, IProjectPropertiesContext projectPropertiesContext)
+            : base(configuredProject, projectPropertiesContext)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectProperties"/> class.
+        /// </summary>
+        public ProjectProperties(ConfiguredProject configuredProject, UnconfiguredProject unconfiguredProject)
+            : base(configuredProject, unconfiguredProject)
+        {
+        }
+
         public new ConfiguredProject ConfiguredProject
         {
             get { return base.ConfiguredProject; }
