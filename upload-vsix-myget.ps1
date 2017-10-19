1c6a6875b4b9adea26e89f8a5d287a4c2ae8b859
José Simões <josesimoes@users.noreply.github.com>
2017-10-19 09:22:43 +0100

    Add PowerShell script to upload VSIX to MyGet feed

---
+++ upload-vsix-myget.ps1
@@ -0,0 +1,16 @@
+$artifactsSearchPattern = "./*.vsix"
+$artifactsCollection = (Get-ChildItem $artifactsSearchPattern -Recurse)
+
+$vsixMyGetUploadEndpoint = "https://www.myget.org/F/nanoframework-dev/vsix/upload"
+
+# for the environment variable MyGetToken to work here it has to be set in AppVeyor UI
+
+foreach($file in $artifactsCollection)
+{
+    'Uploading VSIX package to MyGet feed...' | Write-Host -ForegroundColor White -NoNewline
+
+    $webClient = New-Object System.Net.WebClient
+    $webClient.Headers.add('X-NuGet-ApiKey', $env:MyGetToken)
+    $webClient.UploadFile($vsixMyGetUploadEndpoint, 'POST', $file)
+    'OK' | Write-Host -ForegroundColor Green
+}
