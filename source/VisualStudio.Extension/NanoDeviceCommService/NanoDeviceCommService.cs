7edb97ae732364d1a27771ebd8dd1b7a7fec3b3a
José Simões <jose.simoes@eclo.solutions>
2017-10-31 10:44:20 +0000

    Major rework on Device Explorer
    - Add `NanoDeviceCommService` VS service provider to hold debug client
    - Update VS package to be async and load new service
    - Complete rework on Device Explorer view model
    - Major rework on Device Explorer menu command manager
    - Improve auto-connect feature
    - Improve reconnect after deployment feature
    - Update debugger Nuget package to 0.5.0-preview023
    - Update submodule nf-debug @ 30e20e99
    - Bump VS extension to 0.1.68
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/VisualStudio.Extension/NanoDeviceCommService/NanoDeviceCommService.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright (c) 2017 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using Microsoft.VisualStudio.Shell;
+using nanoFramework.Tools.Debugger;
+using nanoFramework.Tools.Debugger.WireProtocol;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace nanoFramework.Tools.VisualStudio.Extension
+{
+    public class NanoDeviceCommService : SNanoDeviceCommService, INanoDeviceCommService
+    {
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider _serviceProvider;
+
+        public NanoDeviceBase Device { get; internal set; } = null;
+
+        public PortBase DebugClient { get; internal set; }
+
+        public NanoDeviceCommService(Microsoft.VisualStudio.Shell.IAsyncServiceProvider provider)
+        {
+            _serviceProvider = provider;
+        }
+
+        public async System.Threading.Tasks.Task CreateDebugClientsAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            // launches the serial client and service
+            // create serial instance WITHOUT app associated because we don't care of app life cycle in VS extension
+            DebugClient = PortBase.CreateInstanceForSerial("", null);
+        }
+
+        public TaskAwaiter GetAwaiter()
+        {
+            return new TaskAwaiter();
+        }
+
+        public bool SelectDevice(string deviceId = null)
+        {
+            // check if this device is available
+            var device = DebugClient.NanoFrameworkDevices.First(d => d.Description == deviceId);
+
+            if (device != null)
+            {
+                Device = device;
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> ConnectToAsync(string deviceId = null, int timeout = 5000)
+        {
+            if (deviceId == null)
+            {
+                return await Device.DebugEngine.ConnectAsync(timeout, true);
+            }
+            else
+            {
+                return await DebugClient.NanoFrameworkDevices.First(d => d.Description == deviceId).DebugEngine.ConnectAsync(timeout, true);
+            }
+        }
+    }
+}
--- source/VisualStudio.Extension/NanoDeviceCommService/SNanoDeviceCommService.cs
+++ source/VisualStudio.Extension/NanoDeviceCommService/SNanoDeviceCommService.cs
@@ -3,13 +3,15 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace nanoFramework.Tools.VisualStudio.Extension
 {
-    public enum ConnectionState
+    public interface SNanoDeviceCommService
     {
-        None = 0,
-        Connected,
-        Connecting,
-        Disconnected
     }
 }
