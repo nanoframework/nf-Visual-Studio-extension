f110c050f79129d81da3a23f19b78da13b64039e
Christophe Gerbier <christophe@mikrobusnet.org>
2017-12-20 12:32:56 +0100

    Update application template with working code to prevent the main thread from exiting

--- source/CSharp.BlankApplication/Program.cs
+++ source/CSharp.BlankApplication/Program.cs
@@ -6,7 +6,15 @@ namespace $safeprojectname$
     {
         public static void Main()
         {
+	  // Insert your code below this line
 
+		
+          // The main() method has to end with this infinite loop.
+          // Do not use the NETMF style : Thread.Sleep(Timeout.Infinite)
+	  while (true)
+          {
+              Thread.Sleep(200);
+          }
         }
     }
 }
