00b41376a4d5cff7007a717cc1e48d7711d597e2
José Simões <jose.simoes@eclo.solutions>
2018-09-11 17:27:43 +0100

    Binary output file is now processed in msbuild task
    - Add new build task: GenerateBinaryOutputTask
    - Remove code from DeployProvider
    - Update MDP.targets to accomodate this
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/Tools.BuildTasks/GenerateBinaryOutputTask.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+namespace nanoFramework.Tools
+{
+    [Description("GenerateBinaryOutputTaskEntry")]
+
+    public class GenerateBinaryOutputTask : Task
+    {
+        #region public properties for the task
+
+        public string Assembly { get; set; }
+
+        public string AssemblyPE { get; set; }
+
+        public ITaskItem[] AssemblyReferences { get; set; }
+
+
+        /// <summary>
+        /// The name(s) of binary file created.
+        /// </summary>
+        [Output]
+        public ITaskItem FileWritten { get; private set; }
+
+        #endregion
+
+        public override bool Execute()
+        {
+            // report to VS output window what step the build is 
+            Log.LogMessage(MessageImportance.Normal, "Generating binary output file...");
+
+            // default with null, indicating that we've generated nothing
+            FileWritten = null;
+
+            // get paths for PE files
+            // rename extension .dll with .pe
+            List<string> peCollection = new List<string>();
+            peCollection = AssemblyReferences?.Select(a => { return a.GetMetadata("FullPath").Replace(".dll", ".pe").Replace(".exe", ".pe"); }).ToList();
+
+            // add executable PE
+            peCollection.Add(AssemblyPE);
+
+            // get executable path and file name
+            // rename executable extension .exe with .bin
+            var binOutputFile = Assembly.Replace(".exe", ".bin");
+
+            using (FileStream binFile = new FileStream(binOutputFile, FileMode.Create))
+            {
+                // now we will re-deploy all system assemblies
+                foreach (string peItem in peCollection)
+                {
+                    // append to the deploy blob the assembly
+                    using (FileStream fs = File.Open(peItem, FileMode.Open, FileAccess.Read))
+                    {
+                        long length = (fs.Length + 3) / 4 * 4;
+                        byte[] buffer = new byte[length];
+
+                        fs.Read(buffer, 0, (int)fs.Length);
+
+                        // copy this assembly to the bin file too
+                        binFile.Write(buffer, 0, (int)length);
+                    }
+                }
+            }
+
+            // bin file written
+            FileWritten = new TaskItem(binOutputFile);
+
+            return true;
+        }
+    }
+}
