28b963e96d75e62fb158b2400a35b697656ff882
José Simões <josesimoes@users.noreply.github.com>
2018-09-14 11:46:41 +0100

    Work on changelog generator for develop branch (#295)

---
+++ generate-change-log.ps1
@@ -0,0 +1,13 @@
+# generate cahnge log on tag commit
+if ($env:APPVEYOR_REPO_TAG)
+{
+    # need this to keep ruby happy
+    md c:\tmp
+
+    # generate change log
+    bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:GitVersion_MajorMinorPatch.$env:GitVersion_CommitsSinceVersionSource"
+
+    # updated changelog and the updated assembly info files
+    git add .
+    git commit --amend --no-edit
+}
