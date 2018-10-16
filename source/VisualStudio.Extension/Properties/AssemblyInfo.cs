352c42c6bfd35830e5f9b2779a49ea8fc4f0c458
nfbot <nfbot@users.noreply.github.com>
2018-10-16 11:10:00 +0000

    Update versions for v0.6.6.5
    [version update]

--- source/VisualStudio.Extension/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Extension/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.6.5.2")]
-[assembly: AssemblyVersion("0.6.5.2")]
-[assembly: AssemblyFileVersion("0.6.5.2")]
+// [assembly: AssemblyVersion("0.6.6.5")]
+[assembly: AssemblyVersion("0.6.6.5")]
+[assembly: AssemblyFileVersion("0.6.6.5")]
 
-[assembly: AssemblyInformationalVersion("0.6.5-preview002")]
+[assembly: AssemblyInformationalVersion("0.6.6-preview005")]
