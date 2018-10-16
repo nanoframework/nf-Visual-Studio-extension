ba546a4b895ff54b7019bb72522fee980f3e0687
José Simões <josesimoes@users.noreply.github.com>
2018-10-16 09:20:26 +0100

    Improvements on CI and CD scritps (#310)

--- generate-change-log.ps1
+++ generate-change-log.ps1
@@ -1,15 +1,20 @@
-# generate cahnge log on tag commit
-if ($env:appveyor_repo_tag -eq "true")
+# generate change log when build is NOT for a pull-request
+if ($env:appveyor_pull_request_number)
+{
+    'Skip change log processing as this is a PR build...' | Write-Host -ForegroundColor White
+}
+else
 {
     # need this to keep ruby happy
     md c:\tmp
 
     # generate change log
-    bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:GitVersion_MajorMinorPatch.$env:GitVersion_CommitsSinceVersionSource"
+    # version includes commits
+    bundle exec github_changelog_generator --token $env:GitHubToken
 
     # updated changelog and the updated assembly info files
     git add CHANGELOG.md
-    git commit -m "Update CHANGELOG for v$env:GitVersion_MajorMinorPatch.$env:GitVersion_CommitsSinceVersionSource"
+    git commit -m "Update CHANGELOG for v$env:GitVersion_AssemblySemVer"
     # need to wrap the git command bellow so it doesn't throw an error because of redirecting the output to stderr
     "$(git push origin)"
 }
