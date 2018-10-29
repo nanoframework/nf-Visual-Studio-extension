06534f6f3f825339a430aa28f7c5a2807bd3ae49
nfbot <nfbot@users.noreply.github.com>
2018-10-29 12:44:50 +0000

    Update versions for v1.0.1.11 [skip ci]
    [version update]

--- source/VisualStudio.Extension/Properties/AssemblyInfo.cs
+++ source/VisualStudio.Extension/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.1.8")]
-[assembly: AssemblyVersion("1.0.1.8")]
-[assembly: AssemblyFileVersion("1.0.1.8")]
+// [assembly: AssemblyVersion("1.0.1.11")]
+[assembly: AssemblyVersion("1.0.1.11")]
+[assembly: AssemblyFileVersion("1.0.1.11")]
 
-[assembly: AssemblyInformationalVersion("1.0.1-preview008")]
+[assembly: AssemblyInformationalVersion("1.0.1-preview011")]
