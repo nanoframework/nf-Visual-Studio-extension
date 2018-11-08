53b0047d75d673832dcb09c0c0732be38659a4b8
José Simões <jose.simoes@eclo.solutions>
2018-11-08 15:56:08 +0000

    Work CD-CI
    - Improve change log generator.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- generate-change-log.ps1
+++ generate-change-log.ps1
@@ -1,11 +1,8 @@
 # Copyright (c) 2018 The nanoFramework project contributors
 # See LICENSE file in the project root for full license information.
 
-# generate change log when build is NOT a pull-request or not a tag (master OR release)
-if ($env:appveyor_pull_request_number -or 
-    ($env:APPVEYOR_REPO_BRANCH -eq "master" -and $env:APPVEYOR_REPO_TAG -eq 'true') -or
-    ($env:APPVEYOR_REPO_BRANCH -match "^release*" -and $env:APPVEYOR_REPO_TAG -eq 'true') -or
-    $env:APPVEYOR_REPO_TAG -eq "true")
+# skip generating the change log when build is a pull-request or not a tag (can't commit when repo is in a tag)
+if ($env:appveyor_pull_request_number -or $env:APPVEYOR_REPO_TAG -eq "true")
 {
     'Skip change log processing...' | Write-Host -ForegroundColor White
 }
@@ -17,7 +14,7 @@ else
     if ($env:APPVEYOR_REPO_BRANCH -eq "master" -or $env:APPVEYOR_REPO_BRANCH -match "^release*")
     {
         # generate change log including future version
-        bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:GitVersion_MajorMinorPatch"
+        bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:NBGV_Version"
     }
     else 
     {
@@ -26,8 +23,14 @@ else
         bundle exec github_changelog_generator --token $env:GitHubToken        
     }
 
-    # updated changelog and the updated assembly info files
-    git add CHANGELOG.md
-    git commit -m "Update CHANGELOG for v$env:GitVersion_NuGetVersionV2"
-    git push origin --porcelain -q > $null
+    # updated changelog, if there are any differences
+    $logDif = git diff CHANGELOG.md
+
+    if($logDif -ne $null)
+    {
+        git add CHANGELOG.md
+        git commit -m "Update CHANGELOG for v$env:MyNuGetVersion"
+        # need to wrap the git command bellow so it doesn't throw an error because of redirecting the output to stderr
+        git push origin "HEAD:$env:APPVEYOR_REPO_BRANCH" --porcelain  | Write-Host
+    }
 }
