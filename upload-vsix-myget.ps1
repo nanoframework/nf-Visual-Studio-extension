ebae280a5de186d99b23bce29b7d8500a9051836
José Simões <jose.simoes@eclo.solutions>
2018-11-08 15:58:40 +0000

    Work CD-CI
    - Upload to MyGet feed now happens on all buils (except PRs)
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- upload-vsix-myget.ps1
+++ upload-vsix-myget.ps1
@@ -5,28 +5,19 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER)
     return
 }
 
-# skip upload when this is not a tag
-if ($env:appveyor_repo_tag -eq "true")
+$artifactsSearchPattern = "./*.vsix"
+$artifactsCollection = (Get-ChildItem $artifactsSearchPattern -Recurse)
+
+$vsixMyGetUploadEndpoint = "https://www.myget.org/F/nanoframework-dev/vsix/upload"
+
+# for the environment variable MyGetToken to work here it has to be set in AppVeyor UI
+
+foreach($file in $artifactsCollection)
 {
+    'Uploading VSIX package to MyGet feed...' | Write-Host -ForegroundColor White -NoNewline
 
-    $artifactsSearchPattern = "./*.vsix"
-    $artifactsCollection = (Get-ChildItem $artifactsSearchPattern -Recurse)
-
-    $vsixMyGetUploadEndpoint = "https://www.myget.org/F/nanoframework-dev/vsix/upload"
-
-    # for the environment variable MyGetToken to work here it has to be set in AppVeyor UI
-
-    foreach($file in $artifactsCollection)
-    {
-        'Uploading VSIX package to MyGet feed...' | Write-Host -ForegroundColor White -NoNewline
-
-        $webClient = New-Object System.Net.WebClient
-        $webClient.Headers.add('X-NuGet-ApiKey', $env:MyGetToken)
-        $webClient.UploadFile($vsixMyGetUploadEndpoint, 'POST', $file)
-        'OK' | Write-Host -ForegroundColor Green
-    }
-}
-else 
-{
-    'Skip upload to MyGet, this commit is not a tag commit...' | Write-Host -ForegroundColor White
+    $webClient = New-Object System.Net.WebClient
+    $webClient.Headers.add('X-NuGet-ApiKey', $env:MyGetToken)
+    $webClient.UploadFile($vsixMyGetUploadEndpoint, 'POST', $file)
+    'OK' | Write-Host -ForegroundColor Green
 }
