b4fe6e8a8b5e70bc6002be81640adfa733064da9
José Simões <jose.simoes@eclo.solutions>
2019-01-16 11:00:40 +0000

    Fix deployment issue with repeated assemblies
    - The current implementation with Linq Distinct() was not filtering assemblies in different locations thus causing repeated items in the deployment blob.
    - Add new class DeploymentAssembly.
    - Improve code in DeployProvider to correctly filter distinct assemblies.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ source/VisualStudio.Extension/DeployProvider/DeploymentAssembly.cs
@@ -0,0 +1,21 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Collections.Generic;
+
+namespace nanoFramework.Tools.VisualStudio.Extension
+{
+    public class DeploymentAssembly
+    {
+        public string Path { get; set; }
+        public string Version { get; set; }
+
+        public DeploymentAssembly(string path, string version)
+        {
+            Path = path;
+            Version = version;
+        }
+    }
+}
---
+++ source/VisualStudio.Extension/DeployProvider/DeploymentAssemblyDistinctEquality.cs
@@ -0,0 +1,32 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Collections.Generic;
+
+namespace nanoFramework.Tools.VisualStudio.Extension
+{
+    internal class DeploymentAssemblyDistinctEquality : IEqualityComparer<DeploymentAssembly>
+    {
+        public bool Equals(DeploymentAssembly assembly1, DeploymentAssembly assembly2)
+        {
+            var name1 = System.IO.Path.GetFileName(assembly1.Path);
+            var name2 = System.IO.Path.GetFileName(assembly2.Path);
+
+            if ((name1 == name2) && (assembly1.Version == assembly2.Version))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode(DeploymentAssembly assembly)
+        {
+            return System.IO.Path.GetFileName(assembly.Path).GetHashCode() ^ assembly.Version.GetHashCode();
+        }
+    }
+}
