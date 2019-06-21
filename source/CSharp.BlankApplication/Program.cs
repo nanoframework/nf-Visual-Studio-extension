6d624fd785efe4b345a7487bd09407687c5c74e3
José Simões <jose.simoes@eclo.solutions>
2019-06-21 05:21:55 +0100

    Update project templates (#393)

--- source/CSharp.BlankApplication/Program.cs
+++ source/CSharp.BlankApplication/Program.cs
@@ -10,6 +10,10 @@ namespace $safeprojectname$
             Console.WriteLine("Hello world!");
 
             Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/articles/intro.html
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
     }
 }
