3db4982dd7db4bf951c361574ca64a9f74208559
José Simões <jose.simoes@eclo.solutions>
2019-06-21 05:04:39 +0100

    Update project templates
    - Add comments to console and class lib templates pointing to relevant URLs.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/CSharp.ClassLibrary/Class1.cs
+++ source/CSharp.ClassLibrary/Class1.cs
@@ -4,5 +4,8 @@ namespace $safeprojectname$
 {
 	public class $safeprojectname$
     {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/articles/intro.html
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
     }
 }
