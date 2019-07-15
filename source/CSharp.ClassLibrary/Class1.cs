85ba8f7c327ef3744b8a8a8a7d9732820882f986
kurtnelle <kurtnelle@users.noreply.github.com>
2019-07-15 04:02:24 -0400

    Update documentation URL in Class template (#396)
    ***NO_CI***

--- source/CSharp.ClassLibrary/Class1.cs
+++ source/CSharp.ClassLibrary/Class1.cs
@@ -5,7 +5,7 @@ namespace $safeprojectname$
 	public class $safeprojectname$
     {
         // Browse our samples repository: https://github.com/nanoframework/samples
-        // Check our documentation online: https://docs.nanoframework.net/articles/intro.html
+        // Check our documentation online: https://docs.nanoframework.net/
         // Join our lively Discord community: https://discord.gg/gCyBu8T
     }
 }
