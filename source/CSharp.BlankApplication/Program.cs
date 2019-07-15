19cd2e3d3a5e36d7bade86c34101681407d4c510
kurtnelle <kurtnelle@users.noreply.github.com>
2019-07-15 04:03:28 -0400

    Update documentation URL in application template (#397)
    ***NO_CI***

--- source/CSharp.BlankApplication/Program.cs
+++ source/CSharp.BlankApplication/Program.cs
@@ -12,7 +12,7 @@ namespace $safeprojectname$
             Thread.Sleep(Timeout.Infinite);
 
             // Browse our samples repository: https://github.com/nanoframework/samples
-            // Check our documentation online: https://docs.nanoframework.net/articles/intro.html
+            // Check our documentation online: https://docs.nanoframework.net/
             // Join our lively Discord community: https://discord.gg/gCyBu8T
         }
     }
