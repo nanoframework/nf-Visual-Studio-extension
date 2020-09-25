d089d94181bd070e03d3bd75a352721233ae3fd5
josesimoes <jose.simoes@eclo.solutions>
2020-09-25 17:14:06 +0100

    Update copyright headers
    ***NO_CI***

--- VisualStudio.Extension-2019/IPMaskedTextBox/IPMaskedTextBox.xaml.cs
+++ VisualStudio.Extension-2019/IPMaskedTextBox/IPMaskedTextBox.xaml.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright (c) 2017 The nanoFramework project contributors
+// Copyright (c) .NET Foundation and Contributors
 // Portions Copyright (c) Ruven Salamon. All rights reserved.
 // See LICENSE file in the project root for full license information.
 //
