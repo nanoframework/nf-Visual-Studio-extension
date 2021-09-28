fe27dfdb0661c8cb04b4cdc00bf73d39937d70a0
Lukas Josai <30559213+Luk164@users.noreply.github.com>
2021-09-28 20:15:23 +0200

    Initial work for VS2022 (#647)
    ***NO_CI***

---
+++ VisualStudio.Extension-2022/ToolWindow.DeviceExplorer/SettingsDialog.xaml.cs
@@ -0,0 +1,166 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace nanoFramework.Tools.VisualStudio.Extension
+{
+    using Microsoft.VisualStudio.PlatformUI;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Interaction logic for DeviceExplorerControl.
+    /// </summary>
+    public partial class SettingsDialog : DialogWindow
+    {
+        private static IPAddress _InvalidIPv4 = new IPAddress(0x0);
+
+        public SettingsDialog(string helpTopic) : base(helpTopic)
+        {
+            InitializeComponent();
+            InitControls();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettingsDialog"/> class.
+        /// </summary>
+        public SettingsDialog()
+        {
+            InitializeComponent();
+            InitControls();
+        }
+
+        // init controls
+        private void InitControls()
+        {
+            // set controls according to stored preferences
+            GenerateDeploymentImage.IsChecked = NanoFrameworkPackage.SettingGenerateDeploymentImage;
+            IncludeConfigBlock.IsChecked = NanoFrameworkPackage.SettingIncludeConfigBlockInDeploymentImage;
+            AutoUpdateEnable.IsChecked = NanoFrameworkPackage.SettingAutoUpdateEnable;
+            IncludePrereleaseUpdates.IsChecked = NanoFrameworkPackage.SettingIncludePrereleaseUpdates;
+
+            if (string.IsNullOrEmpty(NanoFrameworkPackage.SettingPathOfFlashDumpCache))
+            {
+                // no cache location specified
+                StoreCacheToProjectOutputPath.IsChecked = true;
+            }
+            else
+            {
+                // user has a path in preferences
+                StoreCacheToUserPath.IsChecked = true;
+                PathOfFlashDumpCache.Text = NanoFrameworkPackage.SettingPathOfFlashDumpCache;
+            }
+
+            PortBlackList.Text = NanoFrameworkPackage.SettingPortBlackList;
+
+            // OK to add event handlers to controls now
+            StoreCacheToUserPath.Checked += StoreCacheLocationChanged_Checked;
+            StoreCacheToUserPath.Unchecked += StoreCacheLocationChanged_Checked;
+
+            // set focus on close button
+            CloseButton.Focus();
+        }
+
+        private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void GenerateDeploymentImage_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // save new state
+            NanoFrameworkPackage.SettingGenerateDeploymentImage = (sender as ToggleButton).IsChecked ?? false;
+        }
+
+        private void IncludeConfigBlock_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // save new state
+            NanoFrameworkPackage.SettingIncludeConfigBlockInDeploymentImage = (sender as ToggleButton).IsChecked ?? false;
+        }
+
+        private void StoreCacheLocationChanged_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(StoreCacheToProjectOutputPath.IsChecked.GetValueOrDefault())
+            {
+                // cache location is project output
+                // save setting by clearing the path
+                NanoFrameworkPackage.SettingPathOfFlashDumpCache = "";
+                // clear UI control too
+                PathOfFlashDumpCache.Text = "";
+            }
+            else
+            {
+                NanoFrameworkPackage.SettingPathOfFlashDumpCache = PathOfFlashDumpCache.Text;
+            }
+        }
+
+        private void ShowFilePicker_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+            // let's try make things easier:
+            // if the current path is set, open folder browser dialog with it
+            if (!string.IsNullOrEmpty(PathOfFlashDumpCache.Text))
+            {
+                folderBrowserDialog.SelectedPath = PathOfFlashDumpCache.Text;
+            }
+
+            // show dialog
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                // looks like we have a valid path
+                // save setting
+                NanoFrameworkPackage.SettingPathOfFlashDumpCache = folderBrowserDialog.SelectedPath;
+                // update UI control too
+                PathOfFlashDumpCache.Text = folderBrowserDialog.SelectedPath;
+            }
+            else
+            {
+                // any other outcome from folder browser dialog doesn't require processing
+            }
+        }
+
+        private void PortBlackList_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // store updated setting
+            NanoFrameworkPackage.SettingPortBlackList = PortBlackList.Text;
+
+            // update debug client
+            List<string> PortList = new List<string>();
+
+            // need to wrap the processing in a try/catch to deal with bad user input/format
+            try
+            {
+                // grab and parse COM port list
+                if (!string.IsNullOrEmpty(PortBlackList.Text))
+                {
+                    PortList.AddRange(PortBlackList.Text.Split(';'));
+                }
+            }
+            catch
+            {
+                // don't care about bad user input/format/etc 
+            }
+
+            NanoFrameworkPackage.NanoDeviceCommService.DebugClient.PortExclusionList = PortList;
+        }
+
+        private void AutoUpdateEnable_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // save new state
+            NanoFrameworkPackage.SettingAutoUpdateEnable = (sender as ToggleButton).IsChecked ?? false;
+        }
+
+        private void IncludePrereleaseUpdates_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // save new state
+            NanoFrameworkPackage.SettingIncludePrereleaseUpdates = (sender as ToggleButton).IsChecked ?? false;
+        }
+    }
+}
---
+++ VisualStudio.Extension-2022/vs2022-marketplace-overview.md
@@ -0,0 +1,32 @@
+# .NET nanoFramework Visual Studio Extension
+
+The .NET nanoFramework Visual Studio extension provides the required tools to allow C# developers to code, deploy to .NET nanoFramework target boards and debug applications running on the target.
+
+## What is nanoFramework?
+
+.NET nanoFramework is an open-source platform that enables the writing of managed code applications for constrained embedded devices like IoT sensors, wearables, academic proof of concept, robotics, hobbyist/makers creations or even complex industrial equipment. 
+
+Developers can harness a powerful and familiar Microsoft Visual Studio IDE and their .NET C# knowledge to quickly write code without having to worry about the low-level hardware intricacies of a microcontroller. Desktop .NET developers will feel at home and are able to use their skills in embedded systems, enlarging the pool of qualified embedded developers.
+
+It includes a reduced version of the .NET Common Language Runtime (CLR) and features a subset of the .NET base class libraries along with the more common APIs of the Universal Windows Platform allowing code reuse from desktop applications, IoT Core and thousands of code examples and open source projects.
+Using Microsoft Visual Studio, a developer can deploy and debug the code directly on the real hardware.
+
+## Supported platforms
+
+We currently have support for ARM Cortex-M cores and ESP32 providing reference targets for several STM32 Nucleo and Discovery boards, ESP32 DevKit C and Netduino 3.
+
+## Starting a new project
+
+![starting-new-project.png](starting-new-project.png)
+
+## A debugging session in progress
+
+![debug-session.png](debug-session.png)
+
+You can find out more in the project GitHub [here](https://github.com/nanoframework).
+
+To report issues go to our issue tracker on GitHub [here](https://github.com/nanoframework/Home/issues).
+
+For conversations, sharing ideas and support please join our Discord community [here](https://discord.gg/gCyBu8T).
+
+Please note that we also offer a VS extension compatible with [VS2017](https://marketplace.visualstudio.com/manage/publishers/vs-publisher-1470366/extensions/nanoframeworkvs2017extension).
