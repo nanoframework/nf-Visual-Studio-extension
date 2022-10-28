8394f6faea019dc960e3b58181c769b3d25f1938
José Simões <jose.simoes@eclo.solutions>
2022-10-28 13:28:40 +0100

    Update Unit Test project template

--- CSharp.TestApplication/UnitTest1.cs
+++ CSharp.TestApplication/UnitTest1.cs
@@ -3,6 +3,12 @@ using nanoFramework.TestFramework;
 
 namespace $safeprojectname$
 {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // You may want to check how to tweak runsettings file and others details.                                //
+    // Please check the detailed documentation at:                                                            //
+    // https://github.com/nanoframework/nanoFramework.TestFramework#usage-of-net-nanoframework-test-framework //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     [TestClass]
     public class Test1
     {
