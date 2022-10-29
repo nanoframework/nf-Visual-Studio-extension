3dc978446649a97cc3afa2327ec8884cc1e174e5
José Simões <jose.simoes@eclo.solutions>
2022-10-29 09:54:46 +0100

    Revert "Update Unit Test project template (#750)"
    This reverts commit 6b833cfbd86eb05a1aa818fe5ff806adb62cf4ac.

--- CSharp.TestApplication/UnitTest1.cs
+++ CSharp.TestApplication/UnitTest1.cs
@@ -3,12 +3,6 @@ using nanoFramework.TestFramework;
 
 namespace $safeprojectname$
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // You may want to check how to tweak runsettings file and others details.                                //
-    // Please check the detailed documentation at:                                                            //
-    // https://github.com/nanoframework/nanoFramework.TestFramework#usage-of-net-nanoframework-test-framework //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     [TestClass]
     public class Test1
     {
