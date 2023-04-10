538a1188e987da8455656a5e6eabce6c83cf4074
José Simões <jose.simoes@eclo.solutions>
2023-04-10 10:50:50 +0100

    Add license and copyright header to messages classes

--- vs-extension.shared/Messages/SelectedNanoDeviceHasChangedMessage.cs
+++ vs-extension.shared/Messages/SelectedNanoDeviceHasChangedMessage.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
 
 namespace vs_extension.shared.Messages
 {
