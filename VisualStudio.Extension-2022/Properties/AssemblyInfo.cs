53c1a215e109c9a831b01e479d23e8ee83812cd0
José Simões <jose.simoes@eclo.solutions>
2023-11-27 11:45:58 +0000

    Fix missing extension icons (#811)

--- VisualStudio.Extension-2022/Properties/AssemblyInfo.cs
+++ VisualStudio.Extension-2022/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Microsoft.VisualStudio.Shell;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,3 +9,4 @@
 [assembly: AssemblyProduct(".NET nanoFramework VisualStudio Extension")]
 [assembly: AssemblyCopyright("Copyright © 2019 .NET nanoFramework contributors")]
 [assembly: System.CLSCompliant(false)]
+[assembly: ProvideCodeBase]
